a603a5680e3de6213ef702118897839071958bce
Franck Aragão <fjunior.aragao@gmail.com>
2016-04-08 10:34:10 -0300

    first commit

---
+++ src/main/java/br/edu/ifpb/analyzerQuestions/App.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.analyzerQuestions;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

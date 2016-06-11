1df63fe1056689573000d59e6204e067cb6c1ea5
Franck Aragão <fjunior.aragao@gmail.com>
2016-06-10 23:53:27 -0300

    config travis CI

--- src/test/java/br/edu/ifpb/analyzerQuestions/DescriptionTest.java
+++ src/test/java/br/edu/ifpb/analyzerQuestions/DescriptionTest.java
@@ -1,12 +1,26 @@
 package br.edu.ifpb.analyzerQuestions;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+
 import org.junit.Test;
 
 public class DescriptionTest {
 	
 	@Test
-	public void test1(){
-		
+	public void testAssertEquals() {
+		assertEquals("failure - strings are not equal", "text", "text");
+	}
+
+	@Test
+	public void testAssertFalse() {
+		assertFalse("failure - should be false", false);
+	}
+
+	@Test
+	public void testAssertNotNull() {
+		assertNotNull("should not be null", new Object());
 	}
 
 }

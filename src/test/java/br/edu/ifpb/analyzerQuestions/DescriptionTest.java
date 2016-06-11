e01ae6d730a5af228d2cce8a057a1da35bb3420f
Franck Aragão <fjunior.aragao@gmail.com>
2016-06-10 23:36:25 -0300

    config travis CI

---
+++ src/test/java/br/edu/ifpb/analyzerQuestions/DescriptionTest.java
@@ -0,0 +1,12 @@
+package br.edu.ifpb.analyzerQuestions;
+
+import org.junit.Test;
+
+public class DescriptionTest {
+	
+	@Test
+	public void test1(){
+		
+	}
+
+}
---
+++ src/test/java/br/edu/ifpb/analyzerQuestions/TitleTest.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.analyzerQuestions;
+
+import org.junit.Test;
+
+public class TitleTest {
+	
+	
+	@Test
+	public void t1(){
+		
+	}
+
+}
